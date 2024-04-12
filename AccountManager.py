37b6a3b94e2f5869e27d2386dda149f92cd938dd
NazariiKon <nazar.konechniy2@gmail.com>
2024-04-12 12:06:23 +0100

    add log out

--- AccountManager.py
+++ AccountManager.py
@@ -4,23 +4,24 @@ from Account import Account
 
 class AccountManager: 
     file_name = ""
-    accounts = []
+    #accounts = []
     def __init__(self, name):
         self.file_name = name
-        self.accounts = self.read_accounts_from_dictionary()
+        #self.accounts = self.read_accounts_from_dictionary()
     
     def create_account(self, name, surname):
         account = Account(name, surname, random.randint(10000000000000000000, 99999999999999999999), 0, 2, surname)
         self.write_account_to_dictionary(account)
         return account
 
-    def read_accounts_from_dictionary(self):
-        accounts = []
-        with open(self.file_name, 'r') as file:
-            for line in file:
-                data = line.split()
-                accounts.append(Account(*data))
-        return accounts
+    # it's not secure
+    # def read_accounts_from_dictionary(self):
+    #     accounts = []
+    #     with open(self.file_name, 'r') as file:
+    #         for line in file:
+    #             data = line.split()
+    #             accounts.append(Account(*data))
+    #     return accounts
     
     def write_account_to_dictionary(self, account): 
         with open(self.file_name, 'a') as file:
@@ -45,11 +46,11 @@ class AccountManager:
         self.write_account_to_dictionary(account)
 
     def search_account(self, name, surname):
-        for account in self.accounts:
-            if account.name == name:
-                if account.surname == surname:
-                    return account
-        
+        with open(self.file_name, 'r') as file:
+            for line in file:
+                if name in line and surname in line:
+                    data = line.split()
+                    return Account(*data)
         return None
 
         
--- CBFET Bank.py
+++ CBFET Bank.py
@@ -11,7 +11,8 @@ def print_main_menu(name, surname):
     print("4. Change Password")
     print("5. Calculate interest")
     print("6. Print statement")
-    print("7. Quit")
+    print("7. Log out")
+    print("8. Quit")
     
 def print_login_menu():
     print("\033c", end='') # clear console
@@ -51,35 +52,46 @@ def authentication(account_manager):
         
 file_name = 'dictionary.txt'
 account_manager = AccountManager(file_name)
-account = authentication(account_manager)
-
 while True:
-    print_main_menu(account.name, account.surname)
-    if(account.password == account.surname): print("You need to change your password!")
-    choice = input()
+    account = authentication(account_manager)
 
-    match choice: 
-        case '1': 
-            account.check_balance()
-        case '2':
-            amount = float(input("Enter the amount you want to lodge to your account: "))
-            account.lodge_funds(amount)
-            account_manager.update_account_info_in_dictionary(account)
+    while True:
+        print_main_menu(account.name, account.surname)
+        if(account.password == account.surname): print("You need to change your password!")
+        choice = input()
 
-        case '3':
-            amount = float(input("Enter the amount you want to withdraw from your account: "))
-            account.withdraw_funds(amount)
-            account_manager.update_account_info_in_dictionary(account)
+        match choice: 
+            case '1': 
+                account.check_balance()
+            case '2':
+                try:
+                    amount = float(input("Enter the amount you want to lodge to your account: "))
+                except:
+                    input("You have entered an incorrect amount")
+                    continue
+                account.lodge_funds(amount)
+                account_manager.update_account_info_in_dictionary(account)
 
-        case '4':
-            account.change_password()
-            account_manager.update_account_info_in_dictionary(account)
-        case '5':
-            account.calculate_interest(12)
-        case '6':
-            account.print_statement()
-        case '7':
-            exit()
-        case _:
-            input("Invalid option. Please try again.")
-            continue
+            case '3':
+                try:
+                    amount = float(input("Enter the amount you want to withdraw from your account: "))
+                except:
+                    input("You have entered an incorrect amount")
+                    continue
+                account.withdraw_funds(amount)
+                account_manager.update_account_info_in_dictionary(account)
+
+            case '4':
+                account.change_password()
+                account_manager.update_account_info_in_dictionary(account)
+            case '5':
+                account.calculate_interest(12)
+            case '6':
+                account.print_statement()
+            case '7':
+                break
+            case '8':
+                exit()
+            case _:
+                input("Invalid option. Please try again.")
+                continue
